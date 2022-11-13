455d53a29f64a2d7c5d27c149cecf4e43dedea07
baekg6 <baekg.dev@gmail.com>
2022-11-14 01:58:10 +0900

    Feat : 상태에 맞는 화면 노출

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import { useAuthContext } from './hooks/useAuthContext';
 import Home from './pages/home/Home';
@@ -6,16 +6,26 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
 function App() {
-    const { isAuthReady } = useAuthContext();
+    const { isAuthReady, user } = useAuthContext();
     return (
         <div className="App">
+            {/* 인증 정보를 가져온 후 렌더링 */}
             {isAuthReady ? (
                 <BrowserRouter>
                     <Nav />
                     <Routes>
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/login" element={<Login />}></Route>
-                        <Route path="/signup" element={<Signup />}></Route>
+                        <Route
+                            path="/"
+                            element={user ? <Home /> : <Navigate replace={true} to="/login"></Navigate>}
+                        ></Route>
+                        <Route
+                            path="/login"
+                            element={!user ? <Login /> : <Navigate replace={true} to="/"></Navigate>}
+                        ></Route>
+                        <Route
+                            path="/signup"
+                            element={!user ? <Signup /> : <Navigate replace={true} to="/"></Navigate>}
+                        ></Route>
                     </Routes>
                 </BrowserRouter>
             ) : (
