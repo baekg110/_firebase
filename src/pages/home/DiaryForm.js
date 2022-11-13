b0475591855b3c132098ff761be7eb3b9df2696c
baekg6 <baekg.dev@gmail.com>
2022-11-14 02:42:57 +0900

    Feat : 저장 후 입력서식 비우기

--- src/pages/home/DiaryForm.js
+++ src/pages/home/DiaryForm.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { useFirestore } from '../../hooks/useFirestore';
 
@@ -14,6 +15,13 @@ export default function DiaryForm(uid) {
         }
     };
 
+    useEffect(() => {
+        if (response.success) {
+            setTitle('');
+            setText('');
+        }
+    }, [response.success]); //해당값이 바뀔때만 작동
+
     const handleSubmit = (event) => {
         event.preventDefault();
         // console.log(title, text);
@@ -26,10 +34,10 @@ export default function DiaryForm(uid) {
                 <fieldset>
                     <legend>일기쓰기</legend>
                     <label htmlFor="tit">일기 제목: </label>
-                    <input id="tit" type="text" required onChange={handleData} />
+                    <input id="tit" type="text" value={title} required onChange={handleData} />
 
                     <label htmlFor="txt">일기 내용: </label>
-                    <textarea id="txt" type="text" required onChange={handleData}></textarea>
+                    <textarea id="txt" type="text" value={text} required onChange={handleData}></textarea>
 
                     <button type="submit">저장하기</button>
                 </fieldset>
