18a64e2dfd3faca236fea2277be19be3106dd597
baekg6 <baekg.dev@gmail.com>
2022-11-14 01:41:51 +0900

    Feat : 로그인 기능 추가

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import styles from "./Login.module.css";
+import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
+import styles from './Login.module.css';
 
 export default function Login() {
     // form의 데이터를 관리
-    const [email, setEmail] = useState(""); //기본값을 빈 문자열로 설정
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState(''); //기본값을 빈 문자열로 설정
+    const [password, setPassword] = useState('');
+    const { error, isPending, login } = useLogin();
 
     const handleData = (event) => {
-        if (event.target.type === "email") {
+        if (event.target.type === 'email') {
             setEmail(event.target.value);
-        } else if (event.target.type === "password") {
+        } else if (event.target.type === 'password') {
             setPassword(event.target.value);
         }
     };
@@ -17,6 +19,7 @@ export default function Login() {
     const handleSubmit = (event) => {
         event.preventDefault(); // Submit 기본 이벤트 블락 -> 페이지 리로딩 방지
         console.log(email, password);
+        login(email, password);
     };
     return (
         <form className={styles.login_form} onSubmit={handleSubmit}>
@@ -28,9 +31,13 @@ export default function Login() {
                 <label htmlFor="myPassWord ">Password : </label>
                 <input type="password" id="myPassWord" required value={password} onChange={handleData} />
 
-                <button type="submit" className={styles.btn}>
-                    로그인
-                </button>
+                {!isPending && (
+                    <button type="submit" className={styles.btn}>
+                        로그인
+                    </button>
+                )}
+                {isPending && <strong>로그인 진행중입니다...</strong>}
+                {error && <strong>{error}</strong>}
             </fieldset>
         </form>
     );
