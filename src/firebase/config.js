bc0b4cb47b1b8e01bea4e75aac41d6dbad9462eb
baekg6 <baekg.dev@gmail.com>
2022-11-14 02:38:44 +0900

    Feat : Firestore 문서 추가 훅 생성

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // 계정과 관련되어 있는 firebase 설정내용
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, Timestamp } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const appAuth = getAuth();
 const appFireStore = getFirestore(app);
+const timestamp = Timestamp;
 
-export { appAuth, appFireStore };
+export { appAuth, appFireStore, timestamp };
